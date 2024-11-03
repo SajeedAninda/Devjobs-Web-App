89e435134c046a6aac20b5a2a99213371b131ccf
SajeedAninda <sajaninda@gmail.com>
2024-11-03 16:49:47 +0600

    Added dark mode and completed the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./navbar.css";
 import logo from "../../assets/desktop/logo.svg"
 import moonIcon from "../../assets/desktop/icon-moon.svg";
 import sunIcon from "../../assets/desktop/icon-sun.svg";
 
 const Navbar = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+        setIsDarkMode(!isDarkMode);
+        document.documentElement.classList.toggle('dark', !isDarkMode);
+    };
+
     return (
-        <nav className='px-32 pt-10 pb-24 w-full navbar flex justify-between items-center'>
+        <nav className='px-32 pt-10 pb-24 w-full navbar flex justify-between items-center bg-[#e7e8e9] dark:bg-[#121721]'>
             <div>
                 <img src={logo} alt="" />
             </div>
 
             <div className='flex items-center gap-3'>
-                <img className='w-[20px]' src={sunIcon} alt="" />
-                <button className='bg-white rounded-[40px] w-[44px] h-[22px] flex items-center px-1 py-1 group'>
-                    <div className='bg-[#5964e0] w-[15px] h-[15px] rounded-full group group-hover:opacity-55 transition-all delay-75 ease-in-out'>
+                <img className='w-[20px]' src={sunIcon} alt="Sun Icon" />
+                <button
+                    onClick={toggleDarkMode}
+                    className={`bg-white rounded-[40px] w-[44px] h-[22px] flex items-center px-1 py-1 group`}
+                >
+                    <div className={`transform transition-transform duration-300 ease-in-out ${isDarkMode ? 'translate-x-[22px]' : 'translate-x-0'} bg-[#5964e0] w-[15px] h-[15px] rounded-full group group-hover:opacity-55 transition-all delay-75`}>
                     </div>
                 </button>
-                <img className='w-[20px]' src={moonIcon} alt="" />
+                <img className='w-[20px]' src={moonIcon} alt="Moon Icon" />
             </div>
         </nav>
     );

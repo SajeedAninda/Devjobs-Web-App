e8daec215e36d63d49a664e7fbad8a75993d9c88
SajeedAninda <sajaninda@gmail.com>
2024-11-12 16:54:04 +0600

    Completed role and content section

--- src/Components/JobDetails/JobDetails.jsx
+++ src/Components/JobDetails/JobDetails.jsx
@@ -19,7 +19,7 @@ const JobDetails = () => {
     console.log(matchedJob)
 
     return (
-        <div className='bg-[#e7e8e9] h-screen dark:bg-[#121721] relative py-24'>
+        <div className='bg-[#e7e8e9] h-fit dark:bg-[#121721] relative py-24'>
             <div className="floatingBar bg-white dark:bg-[#19202d] w-[60%] h-[23vh] mx-auto shadow-lg rounded-lg absolute -top-[80px] left-1/2 transform -translate-x-1/2 flex items-center">
                 <div className='w-[20%] h-full flex justify-center items-center rounded-l-lg' style={{ backgroundColor: matchedJob?.logoBackground }}>
                     <img
@@ -61,14 +61,51 @@ const JobDetails = () => {
                         <h2 className='text-[28px] text-[#121721] dark:text-white font-bold'>
                             {matchedJob?.position}
                         </h2>
-                        <button className='px-8 py-3 rounded-md text-white text-[16px] font-semibold bg-[#5964e0] hover:bg-[#949beb] transition duration-200 delay-75 ease-in-out'>
+                        <Link to={`${matchedJob?.apply}`} className='px-8 py-3 rounded-md text-white text-[16px] font-semibold bg-[#5964e0] hover:bg-[#949beb] transition duration-200 delay-75 ease-in-out'>
                             Apply Now
-                        </button>
+                        </Link>
                     </div>
                     <p className='mt-2 text-[14px] text-[#5964e0] font-semibold'>
                         {matchedJob?.location}
                     </p>
+                </div>
 
+                <div>
+                    <p className='mt-8 text-[16px] text-[#6e8098] font-medium leading-8'>
+                        {matchedJob?.description}
+                    </p>
+                </div>
+
+                <div className='mt-8'>
+                    <h3 className='text-[20px] text-[#121721] dark:text-white font-bold'>
+                        Requirements
+                    </h3>
+                    <p className='mt-4 text-[16px] text-[#6e8098] font-medium leading-8'>
+                        {matchedJob?.requirements?.content}
+                    </p>
+                    <ul className='text-[16px] text-[#6e8098] font-medium  list-disc'>
+                        {matchedJob?.requirements?.items.map((item, index) => (
+                            <li key={index} className='text-[#6e8098] mt-4 ml-5 pl-5'>
+                                {item}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className='mt-8'>
+                    <h3 className='text-[20px] text-[#121721] dark:text-white font-bold'>
+                        What You Will Do
+                    </h3>
+                    <p className='mt-4 text-[16px] text-[#6e8098] font-medium leading-8'>
+                        {matchedJob?.role?.content}
+                    </p>
+                    <ol className='text-[16px] text-[#6e8098] font-medium  list-decimal'>
+                        {matchedJob?.role?.items.map((item, index) => (
+                            <li key={index} className='text-[#6e8098] mt-4 ml-5 pl-5'>
+                                {item}
+                            </li>
+                        ))}
+                    </ol>
                 </div>
             </div>
         </div>
